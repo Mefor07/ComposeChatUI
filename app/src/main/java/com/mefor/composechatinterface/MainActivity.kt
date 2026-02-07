69463865fb2cf6f396c3b4f294d077371998d34b
EYWOI2N <ekene.louis.okafor.mefor@digiteqautomotive.com>
2026-02-07 23:23:40 +0100

    Improve layouts with columns and rows

--- app/src/main/java/com/mefor/composechatinterface/MainActivity.kt
+++ app/src/main/java/com/mefor/composechatinterface/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mefor.composechatinterface.ui.theme.ComposeChatInterfaceTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,8 +38,26 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message){
-    Text(msg.author)
-    Text(msg.body)
+    Row{
+        Image(
+          painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+            //set Image size to 40dp
+                .size(40.dp)
+                //Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        //Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 @Preview
